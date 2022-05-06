1248bb831d0a650b8540a5a45c5622a70b301875
porpyry <porpyry@gmail.com>
2022-05-07 06:20:27 +0900

    2

--- config.def.h
+++ config.def.h
@@ -14,6 +14,10 @@ static const char *colors[SchemeLast][2] = {
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines = 0;
 
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 26;
+static unsigned int min_lineheight = 26;
+
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
